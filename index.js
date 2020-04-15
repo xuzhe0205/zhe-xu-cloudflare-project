9834b7a7047dd1f1ef6cf79a50d53b49c1da6b6e
Zhe Xu <zhexu@Zhes-MacBook-Pro.local>
2020-04-15 14:58:21 -0400

    added background

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Cloudflare internship project</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet"></link>
+  </head>
+  <body class="bg-green-200">
+    <div class="w-full h-full flex content-center justify-center mt-8">
+      <div class="bg-white shadow-md rounded px-8 pt-6 py-8 mb-4">
+        <h1 class="block text-grey-800 text-md font-bold mb-2">API</h1>
+        <div class="flex">
+          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-800 leading-tight focus:outline-none focus:shadow-outline" type="text" name="name" placeholder="Your url here"></input>
+          <button class="bg-green-500 hover:bg-green-800 text-white font-bold ml-2 py-2 px-4 rounded focus:outline-none focus:shadow-outline" id="create" type="submit">Generate</button>
+        </div>
+        <div class="mt-4" id="mylist"></div>
+      </div>
+    </div>
+  </body>
+</html>
+`;
+
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  return new Response(html, { headers: { "content-type": "text/html" } });
 }
